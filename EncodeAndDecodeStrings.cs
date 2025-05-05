f2eb8746a52c484110a3a92acf4b5aa793dc53ad
Anthony <anthony.decuns@gmail.com>
2025-05-05 11:17:20 +0200

    Add project files.

---
+++ EncodeAndDecodeStrings.cs
@@ -0,0 +1,48 @@
+﻿namespace neetCode
+{
+    internal class EncodeAndDecodeStrings
+    {
+        int key = 2;
+        List<int> lengthEncodedWords = new();
+        
+
+        public string Encode(IList<string> strs)
+        {
+
+            string s = "";
+            foreach (string word in strs)
+            {
+                int length = 0;
+                for (int i = 0; i <= word.Length - 1; i++)
+                {
+                    s += (char)(word[i] + key);
+                    length++;
+                }
+                lengthEncodedWords.Add(length);
+            }
+            return s;
+        }
+
+        public List<string> Decode(string s)
+        {
+            List<string> decodedStrings = new();
+            string newWord = "";
+            int n = 0;
+            int i = 0;
+            foreach (char c in s)
+            {
+                newWord += (char)(c - key);
+                i++;
+                if (i == lengthEncodedWords[n])
+                {
+                    decodedStrings.Add(newWord);
+                    newWord = "";
+                    n++;
+                    i = 0;
+                }
+            }
+            lengthEncodedWords = new();
+            return decodedStrings;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿namespace neetCode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ManagerEncodeAndDecodeStrings();
+        }
+
+        public static void ManagerEncodeAndDecodeStrings()
+        {
+            List<string> InputOne = new List<string>(["neet", "code", "love", "you"]);
+            List<string> InputTwo = new List<string>(["we", "say", ":", "yes"]);
+
+            EncodeAndDecodeStrings encodeAndDecodeStrings = new EncodeAndDecodeStrings();
+
+            var _one = encodeAndDecodeStrings.Encode(InputOne);
+            foreach(string str in encodeAndDecodeStrings.Decode(_one))
+            {
+                Console.Write(str); 
+            }
+            Console.WriteLine("");
+
+            var _two = encodeAndDecodeStrings.Encode(InputTwo);
+            foreach (string str in encodeAndDecodeStrings.Decode(_two))
+            {
+                Console.Write(str);
+            }
+            Console.WriteLine("");
+        }
+
+    }
+}
