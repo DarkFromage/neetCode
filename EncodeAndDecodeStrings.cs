e3ee743a28f2651aa8152287315217158cbf87cc
Anthony <anthony.decuns@gmail.com>
2025-05-06 11:34:15 +0200

    correct

--- EncodeAndDecodeStrings.cs
+++ EncodeAndDecodeStrings.cs
@@ -5,32 +5,56 @@
         int key = 2;
         List<int> lengthEncodedWords = new();
         
-
         public string Encode(IList<string> strs)
         {
+            if (strs.Count == 0) return null;
 
             string s = "";
             foreach (string word in strs)
             {
-                int length = 0;
-                for (int i = 0; i <= word.Length - 1; i++)
+                if (word.Length == 0)
                 {
-                    s += (char)(word[i] + key);
-                    length++;
+                    lengthEncodedWords.Add(0);
                 }
-                lengthEncodedWords.Add(length);
+                else
+                {
+                    int length = 0;
+                    for (int i = 0; i <= word.Length - 1; i++)
+                    {
+                        s += (char)(word[i] + key);
+                        length++;
+                    }
+                    lengthEncodedWords.Add(length);
+                }
             }
             return s;
         }
 
         public List<string> Decode(string s)
         {
+            if (s == null)
+            {
+                return new List<string>();
+            }
+
             List<string> decodedStrings = new();
+
+            if (s.Length == 0)
+            {
+                decodedStrings.Add("");
+                return decodedStrings;
+            }
+
             string newWord = "";
             int n = 0;
             int i = 0;
             foreach (char c in s)
             {
+                if(lengthEncodedWords[n] == 0)
+                {
+                    decodedStrings.Add("");
+                    n++;
+                }
                 newWord += (char)(c - key);
                 i++;
                 if (i == lengthEncodedWords[n])
--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@
 
         public static void ManagerEncodeAndDecodeStrings()
         {
-            List<string> InputOne = new List<string>(["neet", "code", "love", "you"]);
+            List<string> InputOne = new List<string>([""]);
             List<string> InputTwo = new List<string>(["we", "say", ":", "yes"]);
 
             EncodeAndDecodeStrings encodeAndDecodeStrings = new EncodeAndDecodeStrings();
@@ -17,14 +17,15 @@
             var _one = encodeAndDecodeStrings.Encode(InputOne);
             foreach(string str in encodeAndDecodeStrings.Decode(_one))
             {
-                Console.Write(str); 
+                Console.Write(str + " - "); 
             }
             Console.WriteLine("");
 
             var _two = encodeAndDecodeStrings.Encode(InputTwo);
+            Console.WriteLine(_two);
             foreach (string str in encodeAndDecodeStrings.Decode(_two))
             {
-                Console.Write(str);
+                Console.Write(str + " - ");
             }
             Console.WriteLine("");
         }
